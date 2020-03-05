34180376a855658e7f29e1a9139dc56abcbf24f3
Jerome Joof <jeromejoof@yahoo.com>
2020-03-05 12:02:25 -0800

    added investing,nutrition,feeeding activities

--- BabyTracker/app/src/main/java/com/example/babytracker/NutritionActivity.java
+++ BabyTracker/app/src/main/java/com/example/babytracker/NutritionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FeedingActivity extends AppCompatActivity {
+public class NutritionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feeding);
+        setContentView(R.layout.activity_nutrition);
     }
 }
