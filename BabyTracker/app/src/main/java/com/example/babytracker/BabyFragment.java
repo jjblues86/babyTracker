4521de43b6f5a22dbe730229ccf0359a6ce60805
Jerome Joof <jeromejoof@yahoo.com>
2020-03-10 13:46:31 -0700

    updated gitignore to not ignore baby tracker

---
+++ BabyTracker/app/src/main/java/com/example/babytracker/BabyFragment.java
@@ -0,0 +1,135 @@
+package com.example.babytracker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.babytracker.dummy.DummyContent;
+import com.example.babytracker.dummy.DummyContent.DummyItem;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class BabyFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_PARAM1 = "babyName";
+    private static final String ARG_PARAM2 = "babyDateOfBirth";
+
+    // TODO: Customize parameters
+    private String mBabyName;
+    private String mBabyDateOfBirth;
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public BabyFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static BabyFragment newInstance(String babyName, String babyDateOfBirth) {
+        BabyFragment fragment = new BabyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, babyName);
+        args.putString(ARG_PARAM2, babyDateOfBirth);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mBabyName = getArguments().getString(ARG_PARAM1);
+            mBabyDateOfBirth = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_baby_list, container, false);
+
+        // Set the adapter
+//        if (view instanceof RecyclerView) {
+//            Context context = view.getContext();
+//            RecyclerView recyclerView = (RecyclerView) view;
+//            if (mColumnCount <= 1) {
+//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+//            } else {
+//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+//            }
+//            List<Baby> listOfBabies = new LinkedList<>();
+//            listOfBabies.add(new Baby("Martin", 01/01/01, true));
+//            listOfBabies.add(new Baby("Sergey", 9/20/90, true));
+//            listOfBabies.add(new Baby("Dayne", 5/11/81, false));
+//            listOfBabies.add(new Baby("Romell", 4/01/66, false));
+//            listOfBabies.add(new Baby("Jerome", 01/03/30, true));
+//
+//            recyclerView.setAdapter(new MyBabyRecyclerViewAdapter(listOfBabies, mListener));
+//        }
+//        return view;
+    }
+
+    public void whenButtonIsPressed(Uri uri){
+        if(mListener != null){
+            mListener.onListFragmentInteraction(uri);
+        }
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        }
+//        else
+//            {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnListFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentInteraction(Uri uri);
+    }
+}
---
+++ BabyTracker/app/src/main/java/com/example/babytracker/MyBabyRecyclerViewAdapter.java
@@ -0,0 +1,126 @@
+package com.example.babytracker;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.babytracker.BabyFragment.OnListFragmentInteractionListener;
+import com.example.babytracker.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyBabyRecyclerViewAdapter extends RecyclerView.Adapter<MyBabyRecyclerViewAdapter.MyBabyViewHolder> {
+
+    private final List<Baby> babyList;
+    private final OnListFragmentInteractionListener mListener;
+    private final String TAG = "jj.ViewAdapter";
+
+    public MyBabyRecyclerViewAdapter(List<Baby> babyList, OnListFragmentInteractionListener listener) {
+        this.babyList = babyList;
+        mListener = listener;
+    }
+
+    public static class MyBabyViewHolder extends RecyclerView.ViewHolder{
+        public final View view;
+        public Baby baby;
+        public final TextView babyNameView;
+        public final TextView babyDateOfBirthView;
+
+        public MyBabyViewHolder(View view) {
+            super(view);
+            this.view = view;
+            babyNameView = (TextView) view.findViewById(R.id.name);
+            babyDateOfBirthView = (TextView) view.findViewById(R.id.dateOfBirth);
+//            this.babyNameView = babyNameView;
+//            this.babyDateOfBirthView = babyDateOfBirthView;
+        }
+    }
+
+    //It creates a new row to be added to the table
+    @NonNull
+    @Override
+    public MyBabyRecyclerViewAdapter.MyBabyViewHolder onCreateViewHolder( @NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_baby, parent, false);
+
+        //this gives me access to the view
+        final MyBabyViewHolder viewHolder = new MyBabyViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onClickOnBabyCallback(viewHolder.baby);
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyBabyViewHolder holder, int position) {
+        holder.babyNameView.setText(babyList.get(position).name);
+        holder.babyDateOfBirthView.setText("$" + babyList.get(position).dateOfBirth);
+        holder.baby = babyList.get(position);
+    }
+
+    //Given the holder and the position index, fill in that view with right data for that position
+//    @Override
+//    public void onBindViewHolder(final ViewHolder holder, int position) {
+//        holder.babyNameView = mValues.get(position);
+//        holder.mNameView.setText(mValues.get(position).getName());
+////        holder.mDateOfBirthView.setText("" + mValues.get(position).getDateOfBirth());
+////        holder.mImmunizationView.(mValues.get(position).getImmunization());
+//
+//
+//        holder.mView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.i(TAG, "it was clicked");
+////                if (null != mListener) {
+////                    // Notify the active callbacks interface (the activity, if the
+////                    // fragment is attached to one) that an item has been selected.
+////                    mListener.onListFragmentInteraction(holder.mItem);
+////                }
+//            }
+//        });
+//    }
+
+    @Override
+    public int getItemCount() {
+
+        return babyList.size();
+    }
+
+    public static interface OnListFragmentInteractionListener{
+        public void onClickOnBabyCallback(Baby baby);
+    }
+
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        public final View mView;
+//        public final TextView mNameView;
+//        public final TextView mDateOfBirthView;
+////        public final TextView mImmunizationView;
+//        public Baby mItem;
+//
+//        public ViewHolder(View view) {
+//            super(view);
+//            mView = view;
+//            mNameView = (TextView) view.findViewById(R.id.name);
+//            mDateOfBirthView = (TextView) view.findViewById(R.id.dateOfBirth);
+////            mImmunizationView = (TextView) view.findViewById(R.id.immunization);
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return super.toString() + " '" + mDateOfBirthView.getText() + "'";
+//        }
+//    }
+}
